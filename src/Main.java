d0825124432ce0426e59ca23891bee6de2bb25ee
Fatma çelebi <fatmakaygisiz97@gmail.com>
2025-05-29 22:02:55 +0300

    ilk commit

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,13 @@
 public class Main {
     public static void main(String[] args) {
 
-        car car1 =new car();
+        car car1 =new car(2004, "mercedes", "eclass");
 
-        car1.yıl= 2004;
+      /*  car1.yıl= 2004;
         car1.model= "eclass";
         car1.marka= "mercedes";
+
+       */
 
         System.out.println("Arabanın markası:");
         System.out.println(car1.marka + " " + car1.model + " " + car1.yıl);
--- src/car.java
+++ src/car.java
@@ -3,4 +3,12 @@ public class car {
     public int yıl;
     public String marka;
     public String model;
+
+    public car (int yıl, String marka, String model) {
+        this.marka=marka;
+        this.model=model;
+        this.yıl=yıl;
+
+
+    }
 }
